946e21773944c8e0a38f4fc4cf3348ffe263973f
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-10-06 10:03:35 -0300

    Add files via upload

---
+++ Exercicio4-Lista_Condicional.cpp
@@ -0,0 +1,11 @@
+//Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.
+#include <stdio.h>
+int main(){
+	int num1;
+	printf("Digite um numero inteiro: ");
+	scanf("%d", &num1);
+	if ( num1 >0){
+		printf("\nDobro: %d", num1 *2); }
+	else {
+		printf("Triplo: %d", num1 * 3); }
+	return 0; }
