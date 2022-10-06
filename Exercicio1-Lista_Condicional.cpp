f6564c82791892460e52274e85800a007f556f82
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-10-06 09:55:08 -0300

    Add files via upload

---
+++ Exercicio1-Lista_Condicional.cpp
@@ -0,0 +1,18 @@
+/*Faça um algoritmo que simule um caixa eletrônico. O usuário
+informa o valor que deseja sacar, o caixa eletrônico verifica se o
+valor é menor que o saldo disponível e se, o valor é inferior ao
+saldo, informa que o saque será liberado, enquanto que, se o
+valor for maior que o saldo, informar ao usuário que o saque
+excedeu o saldo na conta. Assuma um saldo de $10.000,00 de
+saldo disponível para o saque.*/
+#include <stdio.h>
+	int main(){
+	double saldo;
+	printf("Caixa eletronico\n");
+	printf("\nInforme o valor que deseja sacar: ");
+	scanf("%lf", &saldo);
+		if (saldo >1000000){
+		printf("\nSaque indisponivel por falta de saldo"); }
+			else if (saldo<=1000000){
+			printf("\nSaque liberado"); }
+	return 0; }
