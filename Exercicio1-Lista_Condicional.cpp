c88115f0bb1aa26a080849cbd7d104ab7abb0e32
Rafael Raniere de Oliveira <102255059+Pequeno-Samurai@users.noreply.github.com>
2022-10-23 16:31:53 -0300

    Update Exercicio1-Lista_Condicional.cpp

--- Exercicio1-Lista_Condicional.cpp
+++ Exercicio1-Lista_Condicional.cpp
@@ -1,18 +1,27 @@
-/*FaÁa um algoritmo que simule um caixa eletrÙnico. O usu·rio
-informa o valor que deseja sacar, o caixa eletrÙnico verifica se o
-valor È menor que o saldo disponÌvel e se, o valor È inferior ao
-saldo, informa que o saque ser· liberado, enquanto que, se o
-valor for maior que o saldo, informar ao usu·rio que o saque
+/*Fa√ßa um algoritmo que simule um caixa eletr√¥nico. O usu√°rio
+informa o valor que deseja sacar, o caixa eletr√¥nico verifica se o
+valor √© menor que o saldo dispon√≠vel e se, o valor √© inferior ao
+saldo, informa que o saque ser√° liberado, enquanto que, se o
+valor for maior que o saldo, informar ao usu√°rio que o saque
 excedeu o saldo na conta. Assuma um saldo de $10.000,00 de
-saldo disponÌvel para o saque.*/
+saldo dispon√≠vel para o saque.*/
 #include <stdio.h>
-	int main(){
-	double saldo;
-	printf("Caixa eletronico\n");
-	printf("\nInforme o valor que deseja sacar: ");
-	scanf("%lf", &saldo);
-		if (saldo >1000000){
-		printf("\nSaque indisponivel por falta de saldo"); }
-			else if (saldo<=1000000){
-			printf("\nSaque liberado"); }
-	return 0; }
+
+double saldo;
+
+int main()
+{
+printf("Caixa eletronico\n\nInforme o valor que deseja sacar: ");
+	
+scanf("%lf", &saldo);
+	
+if (saldo >1000000)
+{
+	printf("\nSaque indisponivel por falta de saldo"); 
+}
+
+else if (saldo<=1000000)
+{
+	printf("\nSaque liberado"); 
+}
+return 0; }
